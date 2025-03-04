179521655d86d1f0f2750f36fbcee5ff2af24f51
9ye1 <jastu8646@gmail.com>
2025-03-05 03:32:21 +0900

    전체 폰트 통일화:

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,10 +2,13 @@
 function About() {
   return (
     <div className="w-full max-w-[1000px] h-auto ">
-        <span>모두를 위한 해커톤, 제로톤</span>
-        <div className="w-full max-w-[1000px] h-auto mb-20 p-5 bg-black-100">
-            <span>🚀 단국대에서 개최되는 전국 해커톤 대회!! ‘제로톤(ZeroThon)’ 해커톤 참가자를 모집합니다! 🎉
-            💡 “0부터 시작하는 해커톤”, 개발 경험이 적어도 부담 없이 도전할 수 있는 열린 무대!🔥 단 100시간! 창의적인 아이디어를 현실로 만들고, 실전 프로젝트 경험까지!</span>
+        <span className="font-bold">모두를 위한 해커톤, 제로톤</span>
+        <div className="font-medium w-full max-w-[1000px] h-auto mb-20 p-5 bg-black-100">
+            <span>🚀 단국대에서 개최되는 전국 해커톤 대회!! ‘제로톤(ZeroThon)’ 해커톤 참가자를 모집합니다! 🎉
+            💡 “0부터 시작하는 해커톤”, 개발 경험이 적어도 부담 없이 도전할 수 있는 열린 무대!🔥 단 100시간! 
+            창의적인 아이디어를 현실로 만들고, 실전 프로젝트 경험까지!<br/>
+            다양한 이벤트와 상품이 마련되어 있어 참가자들이 더욱 즐겁게 몰입할 수 있습니다. 
+            도전과 성장, 그리고 즐거움이 함께하는 제로톤에서 당신의 가능성을 펼쳐보세요! 🚀</span>
         </div>
     </div>
     
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        bold: ['PretendardBold', 'sans-serif'],
+        medium: ['PretendardMedium', 'sans-serif'],
+        light: ['PretendardLight', 'sans-serif'],
+        thin: ['PretendardThin', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
