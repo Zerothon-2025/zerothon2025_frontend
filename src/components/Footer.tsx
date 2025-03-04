d1e368158065a25a818e6a2e2c47091e8d3ec3e3
9ye1 <jastu8646@gmail.com>
2025-03-05 05:39:18 +0900

    .

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,9 @@
 function Footer() {
     return (
       <div className=" flex w-full max-w-[1000px] h-auto ">
-          <span className="font-light text-light">주관 : 단국대학교 SW 중심사업단, BBZ<br/>
-                주최 : BBZ<br/></span>
+          <span className="font-medium text-light">
+            주관 : 단국대학교 SW 중심사업단, BBZ<br/>
+            주최 : BBZ<br/></span>
       </div>
       
     );
