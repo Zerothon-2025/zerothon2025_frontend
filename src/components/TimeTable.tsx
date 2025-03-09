12108466bdd8b4228a1b7fa5d9585037fdffee8d
9ye1 <jastu8646@gmail.com>
2025-03-09 23:40:10 +0900

    타임테이블 추가가

---
+++ src/components/TimeTable.tsx
@@ -0,0 +1,19 @@
+function TimeTable() {
+    return (
+      <div className="flex-col w-full max-w-[1000px] h-auto mb-20">
+            <span className="font-bold text-bold">
+            ⏰ 3월 29일 제로톤 행사가 시작됩니다!<br/></span>
+
+        
+          
+          <img className="w-full mt-5 rounded-2xl" src="/images/timetable.jpg" alt="Easyspub"/>
+            
+                
+
+      </div>
+      
+    );
+  }
+  
+  export default TimeTable;
+  
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,6 +3,8 @@ import About from "../components/About";
 import QandA from "../components/QandA";
 import Support from "../components/Support";
 import Footer from "../components/Footer";
+import TimeTable from "../components/TimeTable";
+import Info from "../components/Info";
 
 
 function MainPage() {
@@ -10,6 +12,8 @@ function MainPage() {
       <div className="flex flex-col justify-center items-center pt-5 pb-5 px-20 bg-black">
         <Banner/>
         <About/>
+        <Info/>
+        <TimeTable/>
         <QandA/>
         <Support/>
         <Footer/>
