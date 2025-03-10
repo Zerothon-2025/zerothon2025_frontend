6f9b455bf3ffdb63a5e43d86f786d184bfc5c555
9ye1 <jastu8646@gmail.com>
2025-03-10 13:05:37 +0900

    혜택 및 상품 컴포넌트 추가

---
+++ src/components/Reward.tsx
@@ -0,0 +1,19 @@
+function Reward() {
+    return (
+      <div className="flex-col w-full max-w-[1000px] h-auto mb-20">
+            <span className="font-bold text-bold">
+            🎁 참가자 혜택 및 우승 상품<br/></span>
+
+        
+          
+          <img className="w-full mt-5 rounded-2xl mb-5" src="/images/reward.png" alt="Easyspub"/>
+          <span className="font-medium text-medium">
+          📢 상품 외에 참여자 모두를 위한 제로톤만의 시크릿 기프트가 준비되어있습니다!<br/></span>
+            
+      </div>
+      
+    );
+  }
+  
+  export default Reward;
+  
