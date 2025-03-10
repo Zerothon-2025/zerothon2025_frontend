213e4e2215ee0cbe4fc334835fa40618d06c1d38
9ye1 <jastu8646@gmail.com>
2025-03-10 13:41:54 +0900

    지원 버튼 추가가

---
+++ src/components/ApplyButton.tsx
@@ -0,0 +1,22 @@
+function ApplyButton() {
+
+
+  const handleClick = () => {
+    window.open("https://forms.gle/9dopKP1CVnecSQtx7", "_blank"); // 원하는 URL로 변경
+  };
+
+  return (
+    <div 
+      className="w-full cursor-pointer mb-10" 
+    >
+      <button 
+        onClick={handleClick} 
+        className="w-full h-10 bg-blue-600 text-white font-bold rounded-xl hover:bg-blue-700 transition shadow-lg"
+      >
+        지원하기 
+      </button>
+    </div>
+  );
+}
+
+export default ApplyButton;
