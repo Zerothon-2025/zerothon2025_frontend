d3e58f7621649bda355050a3069fe95f03a03e93
9ye1 <jastu8646@gmail.com>
2025-03-10 13:05:52 +0900

    주관 및 주최 로고 추가

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,25 @@
 
 function Footer() {
     return (
-      <div className=" flex w-full max-w-[1000px] h-auto ">
-          <span className="font-medium text-light">
-            주관 : 단국대학교 SW 중심사업단, BBZ<br/>
-            주최 : BBZ<br/></span>
-      </div>
+      <div className="flex items-center w-full max-w-[1000px] h-auto mt-4">
+
+            <span className="font-medium text-light">
+            주관 </span>
+            <div>
+                    <img src="/logos/schoolLogo.png" alt="Crycheese" className="w-32 h-32 object-contain"/>
+                </div>
+                <div>
+                    <img src="/logos/BBZlogo.png" alt="Easyspub" className="w-32 h-32 object-contain"/>
+                 </div>
+
+                 <div className="flex-grow"></div>
+                 <span className="font-medium text-light">주최<br/></span>
+            <div>
+                    <img src="/logos/BBZlogo.png" alt="Crycheese" className="w-32 h-32 object-contain"/>
+                </div>
+            </div>
       
     );
   }
   
-  export default Footer;
-  
+export default Footer;
