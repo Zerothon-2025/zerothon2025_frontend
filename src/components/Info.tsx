8361ba2c2b6e8c62b2ef94c6170a316145c3a0eb
HyunTaek Oh <oht222@naver.com>
2025-03-10 15:36:56 +0900

    deploy: 배포 관련 의존성 및 baseUrl 세팅

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,24 +1,26 @@
-
 function Info() {
-    return (
-      <div className=" flex-col w-full max-w-[1000px] h-auto mb-20">
+  return (
+    <div className=" flex-col w-full max-w-[1000px] h-auto mb-20">
+      <span className="font-bold text-bold">
+        🗺️ 대회 장소
+        <br />
+      </span>
 
-        <span className="font-bold text-bold">
-        🗺️ 대회 장소<br/></span>
+      <span className="font-medium text-light">
+        경기도 용인시 수지구 죽전로 152 단국대학교 죽전캠퍼스 사범관 206호
+      </span>
 
-        <span className="font-medium text-light">
-            경기도 용인시 수지구 죽전로 152 단국대학교 죽전캠퍼스 사범관 206호</span>
+      <img
+        className="w-full rounded-xl mt-3 mb-2"
+        src="/Images/location.png"
+        alt="Easyspub"
+      />
 
-        <img className="w-full rounded-xl mt-3 mb-2" src="../public/Images/location.png" alt="Easyspub"/>
+      <span className="font-medium text-light text-gray-500">
+        ※ 찾아오시는 상세한 길은 공식 인스타그램을 통해 안내될 예정입니다!
+      </span>
+    </div>
+  );
+}
 
-
-          
-        <span className="font-medium text-light text-gray-500">
-            ※ 찾아오시는 상세한 길은 공식 인스타그램을 통해 안내될 예정입니다!</span>
-      </div>
-      
-    );
-  }
-  
 export default Info;
-  
