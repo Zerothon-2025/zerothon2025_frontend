2721c361ad53a2c4283d327b9b62bc28c43eebf3
9ye1 <jastu8646@gmail.com>
2025-03-11 17:31:27 +0900

    Q&A수정

--- src/components/QandA.tsx
+++ src/components/QandA.tsx
@@ -31,10 +31,12 @@ function QandAItem({ question, answer }: QandAItemProps) {
 function QandA() {
     const questions: QandAItemProps[] = [
         { question: "참가비는 얼마인가요?", answer: "참가비는 '무료'입니다." },
-        { question: "참가 신청은 어떻게 하나요?", answer: "공식 웹사이트에서 신청할 수 있습니다." },
+        { question: "참가 신청은 어떻게 하나요?", answer: "사이트 하단에 지원하기 버튼 눌러주세요!" },
         { question: "개인으로 참가할 수 있나요?", answer: "네, 개인의 경우 랜덤으로 팀이 매칭됩니다." },
         { question: "당일 늦참 가능한가요?", answer: "불가능합니다. 10시 전에 체크인이 모두 마감됩니다." },
         { question: "점심 저녁 다 제공되나요?", answer: "네, 제공됩니다." },
+        { question: "개발이 처음인데 신청해도 될까요?", answer: "환영입니다! 제로톤은 처음 시작하는 분들에게도 경험을 제공하기 위한 행사입니다😊" },
+        { question: "준비물이 따로 있을까요?", answer: "팀별 멀티탭, 개인 노트북, 추가 간식(물 제공, 정수기 O), 즐길 마음, 체력, 열정⚡" },
     ];
 
     return (
