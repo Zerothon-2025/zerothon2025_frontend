e05e09db4b6843fb1abacb7a534d3e3cc5df119f
HyunTaek Oh <oht222@naver.com>
2025-03-14 23:27:29 +0900

    chore: 콘솔 이스터 에그 추가

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+
 import Banner from "../components/Banner";
 import About from "../components/About";
 import QandA from "../components/QandA";
@@ -8,8 +10,13 @@ import Info from "../components/Info";
 import Reward from "../components/Reward";
 import Howto from "../components/Howto";
 import ApplyButton from "../components/ApplyButton.tsx";
+import consoleArt from "../utils/consoleArt.ts";
 
 function MainPage() {
+  useEffect(() => {
+    consoleArt();
+  }, []);
+
   return (
     <div className="relative justify-center">
       <div className="flex flex-col justify-center items-center pt-5 pb-20 px-10 bg-[#050A1F] sm:px-20 sm:pt-5 sm:pb-20">
