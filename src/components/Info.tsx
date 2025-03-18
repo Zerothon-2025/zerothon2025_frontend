fbee4cb30792b2abc79d9063491f00ec8a1634cd
9ye1 <jastu8646@gmail.com>
2025-03-18 16:40:16 +0900

    장소 사진 카드 플립 애니메이션 구현

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,3 +1,5 @@
+import "../styles/flipCard.css";
+
 function Info() {
   return (
     <div className=" flex-col w-full max-w-[1000px] h-auto mb-20">
@@ -10,11 +12,20 @@ function Info() {
         경기도 용인시 수지구 죽전로 152 단국대학교 죽전캠퍼스 사범관 206호
       </span>
 
-      <img
-        className="w-full rounded-xl mt-3 mb-2"
-        src="/images/location.png"
-        alt="Easyspub"
-      />
+      <div className="card">
+        <img
+          className="card-front"
+          src="/images/location.png"
+          alt="location"
+        />
+
+        <img
+          className="card-back"
+          src="/images/placein.png"
+          alt="place"
+        />
+      </div>
+      
 
       <span className="font-medium text-light text-gray-500">
         ※ 찾아오시는 상세한 길은 공식 인스타그램을 통해 안내될 예정입니다!
